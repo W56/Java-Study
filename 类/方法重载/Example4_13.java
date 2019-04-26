465ed8d5761155e36fa09f74f3b5bc3a5e6cdeaa
SH <39229808+0215Wyz@users.noreply.github.com>
2019-04-26 15:58:12 +0800

    Create Example4_13.java

---
+++ 类/方法重载/Example4_13.java
@@ -0,0 +1,23 @@
+public class Example4_13 {
+	
+	public static void main(String[] args) {
+		Circle circle = new Circle();
+		circle.setRadius(196.87);
+		Tixing lader = new Tixing(3,21,9);
+		Student zhang = new Student();
+		System.out.println("zhang计算圆的面积：");
+		double result = zhang.computerArea(circle);
+		System.out.println(result);
+		System.out.println("zhang计算梯形的面积：");
+		result = zhang.computerArea(lader);
+		System.out.println(result);
+	}
+	
+}
+
+/*
+zhang计算圆的面积：
+121699.48226600002
+zhang计算梯形的面积：
+108.0
+*/
