6de4f146dd4f168969545a4589d56638cbea169f
SH <39229808+0215Wyz@users.noreply.github.com>
2019-04-26 15:33:28 +0800

    Create  Example4_12.java

---
+++ 类/方法重载/Example4_12.java
@@ -0,0 +1,28 @@
+class People{
+	float hello(int a,int b) {
+		return a+b;
+	}
+	float hello(long a,int b) {
+		return a+b;
+	}
+	double hello(double a,int b) {
+		return a*b;
+	}
+}
+
+public class Example4_12 {
+
+	public static void main(String[] args) {
+		People tom = new People();
+		System.out.println(tom.hello(10, 20));
+		System.out.println(tom.hello(10L, 20));
+		System.out.println(tom.hello(10.0, 20));
+	}
+
+}
+
+/*
+30.0
+30.0
+200.0
+*/
